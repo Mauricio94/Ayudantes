4e6903b46cbca8ff88bebbd19af76e6ea0434950
Mauricio94 <mauricio94media@hotmail.com>
2016-05-04 09:13:59 -0400

    .

---
+++ version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    local
+ * @subpackage notebookstore
+ * @copyright  2015 Mauricio Meza (mameza@alumnos.uai.cl)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->pluginname = "local_ayudantes";
+$plugin->component = "local_ayudantes";
+$plugin->version = 2016050402;
