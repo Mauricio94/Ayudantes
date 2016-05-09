aeccb7af480f059ff4963c79350b48739ff88222
Mauricio94 <mauricio94media@hotmail.com>
2016-05-09 12:10:33 -0400

    Ultima Actualización

--- misayudantes.php
+++ misayudantes.php
@@ -30,8 +30,39 @@ $urlindex = new moodle_url("/local/notebookstore/index.php");
 $PAGE->set_url($urlindex);
 $PAGE->set_context($context);
 $PAGE->set_pagelayout("standard");
+$PAGE->set_heading("Nombre del Alumno: Patricio Rivera");
+
+$advlink = $CFG->wwwroot.'/local/ayudantes/img/advance.PNG';
+$justlink = $CFG->wwwroot.'/local/ayudantes/img/just.PNG';
 
 echo $OUTPUT->header();
-echo "hola mundo";
+
+echo "Preguntas Corregidas por dia: 10";
+
+$graphs = new html_table();
+
+$graphs->data[] = array(
+		"Avance por dias",
+		"Percepcion de justicia"
+);
+
+$graphs->data[] = array(	
+		"<img src=".$advlink.">",
+		"<img src=".$justlink.">"
+);
+
+$buttonurl = new moodle_url($CFG->dirroot . "/");
+
+echo html_writer::table ( $graphs );
+echo html_writer::nonempty_tag("div", $OUTPUT->single_button($buttonurl, "Atras"), array("align" => "center"));
 
 echo $OUTPUT->footer();
+
+/*select c.marker, u.* 
+from mdl_emarking e
+inner join mdl_emarking_submission s on (s.emarking = e.id)
+inner join mdl_emarking_draft d on (d.submissionid = s.id)
+inner join mdl_emarking_comment c on (c.draft = d.id)
+inner join mdl_user u (u.id = c.marker)
+*/
+
