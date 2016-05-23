fdd7565b702c5508577c31f869f133913fb7af0c
Mauricio94 <mauricio94media@hotmail.com>
2016-05-23 14:28:07 -0400

    Last comments for QA evaluation

--- misayudantes.php
+++ misayudantes.php
@@ -9,6 +9,7 @@
  */
  
 require_once (dirname(dirname(dirname(__FILE__)))."/config.php");
+//This library will be used to generate the graphs, still not implemented
 include $CFG->dirroot.'/lib/graphlib.php';
 global $DB, $PAGE, $OUTPUT;
 
@@ -37,14 +38,17 @@ $PAGE->set_context($context);
 $PAGE->set_pagelayout("standard");
 $PAGE->set_heading(get_string("helper_name", "local_ayudantes").$marker->firstname." ".$marker->lastname);
 
+//Pictures to make an example of how graphs will look there
 $advlink = $CFG->wwwroot.'/local/ayudantes/img/advance.PNG';
 $justlink = $CFG->wwwroot.'/local/ayudantes/img/just.PNG';
 
 echo $OUTPUT->header();
 
 echo get_string("corrected_questions", "local_ayudantes");
+//We make a table only for data structure and order
 $graphs = new html_table();
 
+//We define where graphs and their names should be
 $graphs->data[] = array(
 		get_string("progress_day", "local_ayudantes"),
 		get_string("justice_perception", "local_ayudantes")
@@ -57,6 +61,7 @@ $graphs->data[] = array(
 
 $buttonurl = new moodle_url('/local/ayudantes/ayudantes.php');
 
+//With this we display the table and the button that redirect us to "ayudantes.php
 echo html_writer::table ( $graphs );
 echo html_writer::nonempty_tag("div", $OUTPUT->single_button($buttonurl, "Atras"), array("align" => "center"));
 
